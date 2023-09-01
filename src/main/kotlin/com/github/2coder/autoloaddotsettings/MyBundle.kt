25c4e15bfa9a0b8c164c7baae01ed8419980eec6
GitHub Action <action@github.com>
2023-09-01 11:58:58 +0000

    Template cleanup

--- src/main/kotlin/com/github/2coder/autoloaddotsettings/MyBundle.kt
+++ src/main/kotlin/com/github/2coder/autoloaddotsettings/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.2coder.autoloaddotsettings
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/2coder/autoloaddotsettings/services/MyProjectService.kt
+++ src/main/kotlin/com/github/2coder/autoloaddotsettings/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.2coder.autoloaddotsettings.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.2coder.autoloaddotsettings.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
